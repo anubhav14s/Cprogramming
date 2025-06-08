6d4fb65b58c3832253ee05464641acfaff27e78c
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-08 21:14:45 +0530

    m

--- and_or.c
+++ and_or.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int x, y;
 
     printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &x, &y);
 
     
-    if (a > 0 && b > 0) {
+    if (x > 0 && y > 0) {
         printf("Both numbers are positive (Logical AND is true).\n");
     } else {
         printf("At least one number is not positive (Logical AND is false).\n");
     }
 
     
-    if (a > 0 || b > 0) {
+    if (x > 0 || y > 0) {
         printf("At least one number is positive (Logical OR is true).\n");
     } else {
         printf("Both numbers are not positive (Logical OR is false).\n");
