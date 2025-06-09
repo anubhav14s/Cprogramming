740622f8664861fdf16f0bf5b5e0e2fbf3884cc4
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-09 21:16:09 +0530

    m

--- float_pointer.c
+++ float_pointer.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
-    int cost=3287.97;
-    int *ptr=&cost;
+    int price=3287.97;
+    int *ptr=&price;
     printf("Address of the variable is %u\n",ptr);
-    printf("Address of the variable is %u\n",&cost);
+    printf("Address of the variable is %u\n",&price);
     printf("%d\n",*ptr);
-    printf("%d\n",cost);
-    printf("%d\n",*(&cost));
+    printf("%d\n",price);
+    printf("%d\n",*(&price));
     return 0;
 }
