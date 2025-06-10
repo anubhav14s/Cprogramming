30f1c76ec2610393e949d0627efed40d60c95eb8
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-10 15:13:41 +0530

    m

--- square.c
+++ square.c
@@ -2,16 +2,16 @@
 #include <math.h>
 
 int main() {
-    double num, square;
+    double n1, square;
 
     
     printf("Enter a number: ");
-    scanf("%lf", &num);  
+    scanf("%lf", &n1);  
 
-    square = pow(num, 2);  
+    square = pow(n1, 2);  
 
     
-    printf("The square of %.2lf is %.2lf\n", num, square);
+    printf("The square of %.2lf is %.2lf\n", n1, square);
 
     return 0;
 }
