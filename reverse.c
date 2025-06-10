ba9402040f9fbedc04b5e885a7063da53921dfbf
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-10 15:09:35 +0530

    m

--- reverse.c
+++ reverse.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int num, reversed = 0, digit;
+    int num, reverse = 0, digit;
     printf("Enter a number: ");
     scanf("%d", &num);
     while (num != 0) {
         digit = num % 10;
-        reversed = reversed * 10 + digit;
+        reverse = reverse * 10 + digit;
         num /= 10;
     }
-    printf("Reversed number = %d\n", reversed);
+    printf("Reverse number = %d\n", reverse);
     return 0;
 }
