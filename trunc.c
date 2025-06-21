64b65577ece3acfbf49e800a69a2433f6def751c
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-21 13:56:56 +0530

    t

---
+++ trunc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a = 5.89;
+    double b = -3.14;
+
+    printf("trunc(%.2f) = %.2f\n", a, trunc(a));   
+    printf("trunc(%.2f) = %.2f\n", b, trunc(b));   
+
+    return 0;
+}
